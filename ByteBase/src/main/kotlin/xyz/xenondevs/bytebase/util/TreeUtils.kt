60f858d35a121775afef42a0bcf28b130147c615
ByteZ1337 <0xbytez@gmail.com>
2021-11-03 16:43:22 +0100

    Create TreeUtils.kt

---
+++ ByteBase/src/main/kotlin/xyz/xenondevs/bytebase/util/TreeUtils.kt
@@ -0,0 +1,52 @@
+package xyz.xenondevs.bytebase.util
+
+import org.objectweb.asm.tree.*
+import xyz.xenondevs.bytebase.asm.access.ReferencingAccess
+import xyz.xenondevs.bytebase.jvm.ClassWrapper
+import xyz.xenondevs.bytebase.jvm.VirtualClassPath
+import kotlin.reflect.KClass
+
+fun AnnotationNode.toMap(): Map<String, Any?> {
+    val map = mutableMapOf<String, Any?>()
+    for (i in 0 until this.values.size step 2) {
+        map[this.values[i].toString()] = this.values[i + 1]
+    }
+    return map
+}
+
+val Class<*>.internalName get() = name.replace('.', '/')
+
+val KClass<*>.internalName get() = java.internalName
+
+fun ClassNode.hasAnnotations() = !this.visibleAnnotations.isNullOrEmpty() || !this.invisibleAnnotations.isNullOrEmpty()
+
+val FieldNode.accessWrapper
+    get() = ReferencingAccess(this::access) { access = it }
+
+fun FieldNode.hasAnnotations() = !this.visibleAnnotations.isNullOrEmpty() || !this.invisibleAnnotations.isNullOrEmpty()
+
+val FieldInsnNode.ownerClass
+    get() = VirtualClassPath.getClass(this.owner)
+
+val FieldInsnNode.node
+    get() = this.ownerClass.getField(this.name, this.desc)
+
+val FieldInsnNode.access
+    get() = node?.accessWrapper ?: error("Field $owner.$name does not exist") // ValueAccess(ACC_PRIVATE)
+
+fun MethodNode.isInherited(clazz: ClassWrapper) =
+    clazz.inheritanceTree.superClasses.any { this in it }
+
+val MethodNode.accessWrapper
+    get() = ReferencingAccess(this::access) { access = it }
+
+fun MethodNode.hasAnnotations() = !this.visibleAnnotations.isNullOrEmpty() || !this.invisibleAnnotations.isNullOrEmpty()
+
+val MethodInsnNode.ownerClass
+    get() = VirtualClassPath.getClass(this.owner)
+
+val MethodInsnNode.node
+    get() = this.ownerClass.getMethod(this.name, this.desc)
+
+val MethodInsnNode.access
+    get() = node?.accessWrapper ?: error("Method $owner.$name does not exist") // ValueAccess(ACC_PRIVATE)
