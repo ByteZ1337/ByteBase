d5b165f705bf65eca8ed90a5ed254d22b7e0c79f
ByteZ1337 <0xbytez@gmail.com>
2022-07-10 13:05:34 +0200

    KClass functions

--- ByteBase-Runtime/src/main/kotlin/xyz/xenondevs/bytebase/RuntimeUtils.kt
+++ ByteBase-Runtime/src/main/kotlin/xyz/xenondevs/bytebase/RuntimeUtils.kt
@@ -54,6 +54,24 @@ fun Class<*>.insertInstructions(method: String, instructions: InsnList) {
     INSTRUMENTATION.redefineClasses(ClassDefinition(this, wrapper.assemble()))
 }
 
+//<editor-fold desc="KClass extensions" defaultstate="collapsed">
+
+fun KClass<*>.redefine(transformer: ClassWrapper.() -> Unit) = this.java.redefine(transformer)
+
+fun KClass<*>.redefineMethod(method: String, desc: String, transformer: MethodNode.() -> Unit) = this.java.redefineMethod(method, desc, transformer)
+
+fun KClass<*>.redefineMethod(method: String, transformer: MethodNode.() -> Unit) = this.java.redefineMethod(method, transformer)
+
+fun KClass<*>.setInstructions(method: String, desc: String, instructions: InsnList) = this.java.setInstructions(method, desc, instructions)
+
+fun KClass<*>.setInstructions(method: String, instructions: InsnList) = this.java.setInstructions(method, instructions)
+
+fun KClass<*>.insertInstructions(method: String, desc: String, instructions: InsnList) = this.java.insertInstructions(method, desc, instructions)
+
+fun KClass<*>.insertInstructions(method: String, instructions: InsnList) = this.java.insertInstructions(method, instructions)
+
+//</editor-fold>
+
 fun ClassWrapper.load(): Class<*> {
     return ClassWrapperLoader.DEFAULT.loadClass(this)
 }
